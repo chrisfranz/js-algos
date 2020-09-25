46d96be6e05211f2e79a96e1522f4c3bf5900cb8
chrisfranz <chrisfranz@mac.com>
2020-09-25 15:33:10 -0700

    longest substring complete

--- Med/lengthLongestSubstring.js
+++ Med/lengthLongestSubstring.js
@@ -3,22 +3,25 @@ function lengthLongestSubstring(string) {
 
     let longestSubstringLength = 0;
     for (let a = 0; a < string.length; a++) {
-        const charSet = new Set();
+        let charSet = new Set();
+        // console.log('CHAR SET: ', charSet)
         for (let i = a; i < string.length; i++) {
             
             if (!charSet.has(string[i])) {
-                charSet.add(string[i])
-                console.log(charSet.size)
-            } else {
-                if (charSet.size > lengthLongestSubstring) longestSubstringLength = charSet.size
+                charSet.add(string[i]);
+            } else if (charSet.has(string[i])) {
+                if (charSet.size > longestSubstringLength) longestSubstringLength = charSet.size;
                 break
             }
-            
-        }
 
+            if (i + 1 === string.length) {
+                if (charSet.size > longestSubstringLength) longestSubstringLength = charSet.size;
+            }
+        }
+        
     }
     return longestSubstringLength
 }
 
-const result = lengthLongestSubstring('penis');
+const result = lengthLongestSubstring('abcdabcde');
 console.log('result: ', result);
