2bf9e0725b2eb05ef9d7cdb7884eee756a6412ce
chrisfranz <chrisfranz@mac.com>
2020-10-03 13:49:53 -0700

    n,k

--- dynamic-programming/countStairs.js
+++ dynamic-programming/countStairs.js
@@ -21,7 +21,7 @@ function countStairs(n) {
 
 
 // Time O(n), Space O(1)
-function countStairs2(n) {
+function countStairs2a(n) {
     if (n === 0 || n === 1) return 1;
     
     let a = 1, b = 1;
@@ -40,8 +40,71 @@ function countStairs2(n) {
     return c;
 }
 
+function countStairs2b(n) {
+    if (n === 0 || n === 1) return 1;
+    
+    const cache = [ 1, 1 ];
+
+    let output;
+
+    for (let i = 2; i <= n; i++) {
+        output = cache[0] + cache[1];
+        cache[0] = cache[1]
+        cache[1] = output;
+    }
+
+    const [ s, ms ] = process.hrtime(start);
+    const { heapUsed } = process.memoryUsage();
+    const heapConverted = heapUsed / 1024 / 1024
+    console.log(`Runtime: ${s} sec, ${ms / 1000000} ms`)
+    console.log(`Memory usage: ${Math.round(heapConverted * 100) / 100} MB`)
+    return output;
+}
+
+function countStairs3a(n) {
+    const cache = [ 1, 1, 2 ];
+
+    if (n < 3) return cache[n];
+
+    let output;
+
+    for (let i = 3; i <= n; i++) {
+        output = cache[0] + cache[1] + cache[2];
+        cache[0] = cache[1]
+        cache[1] = cache[2];
+        cache[2] = output;
+    }
+
+    const [ s, ms ] = process.hrtime(start);
+    const { heapUsed } = process.memoryUsage();
+    const heapConverted = heapUsed / 1024 / 1024
+    console.log(`Runtime: ${s} sec, ${ms / 1000000} ms`)
+    console.log(`Memory usage: ${Math.round(heapConverted * 100) / 100} MB`)
+    return output;
+}
+
+function countStairs3b(n, k) {
+    const cache = [ 1, 1 ];
+
+    if (n < 2) return cache[n];
 
 
+    for (let i = 2; i <= n; i++) {
+        for (let j = 1; j <= k; j++) {
+            if (i - j < 0) continue;
+            if (!cache[i]) cache[i] = 0;
+            cache[i] += cache[i - j]
+            // console.log('CACHE: ', cache[i])
+        }
+    }
 
-const result = countStairs2(40000000);
+    const [ s, ms ] = process.hrtime(start);
+    const { heapUsed } = process.memoryUsage();
+    const heapConverted = heapUsed / 1024 / 1024
+    console.log(`Runtime: ${s} sec, ${ms / 1000000} ms`)
+    console.log(`Memory usage: ${Math.round(heapConverted * 100) / 100} MB`)
+    return cache[n];
+}
+
+const result = countStairs3b(4, 3);
 console.log('result: ', result);
