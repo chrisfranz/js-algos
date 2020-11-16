49831a2b47a9cb75e5477b923f3bb6054ce4681c
chrisfranz <chrisfranz@mac.com>
2020-11-16 07:26:58 -0800

    count stairs review

---
+++ dynamic-programming/countStairs2.js
@@ -0,0 +1,81 @@
+// Time O(n), Space O(n)
+
+function countStairs(n) {
+    const cache = [ 1, 1 ];
+
+    if (n <= 1) return cache[n]
+
+    for (let i = 2; i <= n; i++) {
+        cache[i] = cache[i - 1] + cache[i - 2]
+    }
+
+    return cache[n]
+}
+// Time O(n), Space O(1)
+function countStairs2(n) {
+    if (n <= 1) return 1;
+    
+    let a = 1;
+    let b = 1;
+
+    for (let i = 2; i <= n; i++) {
+        const current = a + b;
+        a = b
+        b = current
+    }
+    return b;
+}
+// Time O(n), Space O(n)
+function countStairsNK(n, k) {
+    const cache = new Array(n + 1).fill(0)
+    cache[0] = 1;
+    cache[1] = 1;
+
+    if (n <= 1) return cache[n]
+
+    for (let i = 2; i <= n; i++) {
+        for (let j = 1; j <= k; j++) {
+            if (i - j < 0) continue
+            cache[i] += cache[i - j]
+        }
+        console.log('CACHE: ', cache)
+    }
+    return cache[n]
+}
+
+function countStairsNK(n, k) {
+    const cache = new Array(n + 1).fill(0)
+    cache[0] = 1;
+    cache[1] = 1;
+
+    if (n <= 1) return cache[n]
+
+    for (let i = 2; i <= n; i++) {
+        for (let j = 1; j <= k; j++) {
+            if (i - j < 0) continue
+            cache[i] += cache[i - j]
+        }
+        console.log('CACHE: ', cache)
+    }
+    return cache[n]
+}
+
+function countStairsNKs1(n, k) {
+    const cache = new Array(n + 1).fill(0)
+    cache[0] = 1;
+
+    if (n <= 1) return cache[n]
+
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j < k; j++) {
+            if (i - j < 0) continue
+            cache[i % k] += cache[(i - j) % k]
+        }
+        console.log('CACHE: ', cache)
+    }
+    return cache[n % k]
+}
+
+const result = countStairsNKs1(4, 4);
+
+console.log('RESULT: ', result)
